85c9ecd1e280b619e6d25f36d9e71015bf8c7d39
uniqudev <158754682+uniqudev@users.noreply.github.com>
2025-10-12 20:15:02 +0530

    created project and start add colors theme

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  return runApp(MyApp());
+}
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Smiley Toothy",
+      home: Scaffold(
+        appBar: AppBar(title: Text("Smile"),),
+      ),
+    );
+  }
+}
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+//background colors gradient
+final kMainBackgroundBlueNormal = Color(0xFF0067F6);
+final kMainBackgroundBlueDark = Color(0xFF0041EB);
+//
