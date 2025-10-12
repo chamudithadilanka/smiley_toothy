2a6a025622d63faca651332f2e72193e5dcb932b
uniqudev <158754682+uniqudev@users.noreply.github.com>
2025-10-12 21:42:03 +0530

    Start create plash screen animation

--- lib/screens/splash_screen/dart/splash_screen.dart
+++ lib/screens/splash_screen/dart/splash_screen.dart
@@ -8,7 +8,26 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _scaleAnimation;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 5),
+    );
+
+    _scaleAnimation = Tween<double>(begin: 0.0, end: 2.0).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.easeOutBack),
+    );
+
+    _animationController.forward();
+  }
+
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
@@ -17,14 +36,30 @@ class _SplashScreenState extends State<SplashScreen> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Container(
+            AnimatedContainer(
               height: screenHeight * 1.0,
               width: screenWidth * 1.0,
               decoration: BoxDecoration(
                 gradient: RadialGradient(
-                  colors: [kMainBackgroundBlueNormal, kMainBackgroundBlueDark],
+                  colors: [
+                    kMainBackgroundBlueNormal.withOpacity(1.0),
+                    kMainBackgroundBlueDark,
+                    kMainBackgroundBlueDark,
+                  ],
                   center: Alignment.center,
-                  radius: 0.85,
+                  radius: 0.90,
+                ),
+              ),
+              duration: Duration(seconds: 5),
+              curve: Curves.bounceIn,
+              child: Center(
+                child: ScaleTransition(
+                  scale: _scaleAnimation,
+                  child: Image.asset(
+                    'assets/splash_screen_image.png',
+                    width: 200,
+                    height: 200,
+                  ),
                 ),
               ),
             ),
